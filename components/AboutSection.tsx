7a1a5475c763834e8d5a9f379c0c3eac5a962232
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-03 19:05:18 -0400

    Update AboutSection.tsx

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -6,6 +6,7 @@ export default function AboutSection() {
       <div className="flex justify-between items-center px-10 flex-wrap">
         <div className="max-w-[608px]">
           <h1 className="text-5xl sm:text-[80px]">What is Pandamania?</h1>
+          <br/>
           <p className="text-xl">
             Pandamania is a collection of NFT projects that is set to bring change to the digital and real world. 
             Providing value to the world around us and inspiring love and change for a better tomorrow. 
