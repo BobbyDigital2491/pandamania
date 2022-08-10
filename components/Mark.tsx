c44538cecdcc9f25a0328da7c88a427efdda5faa
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-10 18:34:39 -0400

    Update Mark.tsx

--- components/Mark.tsx
+++ components/Mark.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NftCard } from "./NftCard";
+import { Cow } from "./Cow";
 
 export default function MarketPlace() {
   return (
@@ -14,7 +15,7 @@ export default function MarketPlace() {
                     <NftCard image="mtpan.png" />
                   </div>
                   <div className="py-10">
-                    <NftCard image="cow.png" />
+                    <Cow image="cow.png" />
                   </div>
                 </div>
                 <div className="grid content-center ">
