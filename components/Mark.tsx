a8e6feccd63386adfbb0c4f992c9392f291fa424
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-10 17:59:32 -0400

    Update Mark.tsx

--- components/Mark.tsx
+++ components/Mark.tsx
@@ -1,1 +1,46 @@
+import React from "react";
+import { NftCard } from "./NftCard";
 
+export default function MarketPlace() {
+  return (
+    <>
+      <section className="">
+        <div className="container px-6 py-12 mx-auto">
+          <div className="grid items-center gap-4 xl:grid-cols-5">
+            <div className="p-6 xl:col-span-3">
+              <div className="grid md:grid-cols-2">
+                <div className="grid content-center ml-[3vw] relative -mt-[10rem]">
+                  <div className="py-10">
+                    <NftCard image="crown.png" />
+                  </div>
+                  <div className="py-10">
+                    <NftCard image="guns.png" />
+                  </div>
+                </div>
+                <div className="grid content-center ">
+                  <div className="py-10">
+                    <NftCard image="lady.png" />
+                  </div>
+                  <div className="py-10">
+                    <NftCard image="fish.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="max-w-2xl my-8 space-y-4  xl:col-span-2 text-left">
+              <h2 className="text-5xl sm:text-[80px] font-bold">
+                What is Pandamania?
+              </h2>
+              <p className="">
+             Pandamania is a collection of NFT projects that is set to bring change to the digital and real world. 
+            Providing value to the world around us and inspiring love and change for a better tomorrow. 
+            Bringing value to our NFT holders and allowing them to use us to make a difference in the world. 
+            We are love. We are hope. We are one. We are Pandamania. 
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
