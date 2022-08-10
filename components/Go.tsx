16bd194626135135ee8a1854ed5d709b1cd0294f
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-10 17:22:51 -0400

    Update Go.tsx

--- components/Go.tsx
+++ components/Go.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="min-h-screen sm:my-20 ">
       <div className="flex  w-full flex-wrap xl:flex-nowrap container mx-auto  justify-between  py-[2rem] z-50 ">
         <div className="flex sm:mt-8 flex-col  text-left px-2   ">
-          <img src="919C8D64-B1B2-4E08-B33C-A89C022D015D.PNG" />
+          <img src="bird.png" />
           <h1 className="text-5xl sm:text-[80px]">Project Mission</h1>
           <br/>
           <p className="max-w-lg text-xl ">
