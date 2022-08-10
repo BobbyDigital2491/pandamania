367db446bda0927025cec9cbfb1c40313d6a25d4
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-10 18:15:04 -0400

    Create Zero.tsx

---
+++ components/Zero.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function Zero() {
+  return (
+    <div className="min-h-screen sm:my-20 ">
+      <div className="flex  w-full flex-wrap xl:flex-nowrap container mx-auto  justify-between  py-[2rem] z-50 ">
+        <div className="flex sm:mt-8 flex-col  text-left px-2   ">
+          <img src="new.png" />
+          <h1 className="text-5xl sm:text-[80px]">Project Mission</h1>
+          <br/>
+          <p className="max-w-lg text-xl ">
+           The mission of this project is to promote quality art at an affordable
+          price without straying away from the utilities and benefits of our
+          holders. We will stay committed to bringing value to the project, by
+          focusing on the short and long term goals. Preparing ourselves for
+          long term stability with each short term success one step at a time.
+          Pandamania’s collection consists of multiple projects that share the
+          same common goal. These projects work as a bridge for each other as
+          one focus on community development the other focus on project
+          development. Allowing us to build in both areas effectively and set up
+          a support even throughout a bear market. If conditions change, then
+          the project adapts to the change and continue moving forward.{" "}
+          </p>
+          <div className="flex  items-center sm:justify-center mt-10 sm:m-0 lg:justify-start ">
+            <>
+              
+            </>
+            <>
+              
+            </>
+          </div>
+        </div>
+        <div className="h-[4 relative xl:fit xl:top-16 right-0">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
