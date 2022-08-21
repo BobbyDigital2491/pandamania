447f208d6051d0274770976a02f02e6e70f89184
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-21 11:31:32 -0400

    Update Mike.tsx

--- components/Mike.tsx
+++ components/Mike.tsx
@@ -45,7 +45,7 @@ export default function Like() {
             Elite Mutants giveaways will last 7 days, 1 winner each day!
             <br/>
             <br/>
-            Mutant Pandas forever utilities?
+            <b>Mutant Panda Forever Utilities?</b>
             <br/>
             ALL HAVE GONE MUTANT! TBA! 
 
