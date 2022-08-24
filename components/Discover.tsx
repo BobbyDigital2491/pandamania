e4bf9f1ea1b23e410fe9488e076ef6283e61f999
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-24 17:58:30 -0400

    Update Discover.tsx

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -20,6 +20,7 @@ export default function Discover() {
     <div className="flex flex-col my-10  px-10 ">
       <div className="flex py-10 ">
         <h1 className="text-5xl sm:text-[80px]">The Real Little Fish</h1>
+        <h2 className="text-5xl sm:text-[80px]">Winning Bid 58,000 HBAR</h2>
       </div>
       <div className="flex flex-wrap justify-between  items-center ">
         {/* <div className="grid grid-flow-row grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2  w-full justify-center items-center  grid-rows-4 gap-10"> */}
