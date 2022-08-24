1eb20ce7e23497b27424f79250e4313ccaa170bc
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-08-24 13:02:03 -0400

    Update Discover.tsx

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DftCardWithButton } from "./DftCard";
+import { NftCardWithButton } from "./NftCard";
 
 const data = [
   {
@@ -19,13 +19,13 @@ export default function Discover() {
   return (
     <div className="flex flex-col my-10  px-10 ">
       <div className="flex py-10 ">
-        <h1 className="text-5xl sm:text-[80px]">Our Collection</h1>
+        <h1 className="text-5xl sm:text-[80px]">Owners Name</h1>
       </div>
       <div className="flex flex-wrap justify-between  items-center ">
         {/* <div className="grid grid-flow-row grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2  w-full justify-center items-center  grid-rows-4 gap-10"> */}
 
         {data.map((n, index) => {
-          return  <DftCardWithButton image={n.image} key={index} />;
+          return  <NftCardWithButton image={n.image} key={index} />;
         })}
       </div>
     </div>
