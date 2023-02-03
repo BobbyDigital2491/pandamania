d6e533233189e76f1541d957a90fda0532e43d37
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-02 20:30:23 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -9,7 +9,7 @@ export default function Gang() {
 	<div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
   
   <img src="22.png" width="900" height="400" className="align-center"/>
-  <h1 className="text-5xl font-bold leading-none text-center">Coming Soon</h1>
+  <h1 className="text-5xl font-bold leading-none text-center">Change is here!!!</h1>
 
 	<p className="pt-2 pb-8 text-xl font-medium text-center">
     	Change has come to Pandamania like never seen before. Some pandas are adapting to the furnace around them.
