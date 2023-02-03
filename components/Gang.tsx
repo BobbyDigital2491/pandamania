c647924ad8ffe580a10329252de9d1234a8e891a
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-02 20:28:34 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -28,7 +28,7 @@ export default function Gang() {
 			<br/><br/>
 			<a
                 href="https://docs.google.com/spreadsheets/d/13I4lu9UeU1SbE7wWT7c9xzqnXKV2VCIRBQpA7ZU4-bA/edit#gid=0"
-                className="text-white hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+                className="text-grey-300 hover:text-indigo-500 active:text-indigo-600 transition duration-100"
               >
                 Pandamania X Distribution
               </a>
