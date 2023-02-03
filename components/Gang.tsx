84a673280225eb8d114a9faf9c1f23ae3b64481e
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-02 19:51:07 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -12,14 +12,16 @@ export default function Gang() {
   <h1 className="text-5xl font-bold leading-none text-center">Coming Soon</h1>
 
 	<p className="pt-2 pb-8 text-xl font-medium text-center">
-    	Change has come to Pandamania like never seen before. Some pandas are adapting to the furnace around them. A lot have become Samurais in the fight to survive. Few were infected by failed science experiments and are now known as the elite class. Welcome to change and welcome to Pandamania X!
-	This is a 1600 supply collection, with multi-art styles (type): Samurai (800), Mundane (600), and Mutant (200). Pandamania X main goal is to consolidate 3 of our collections into 1 collection. 
+    	Change has come to Pandamania like never seen before. Some pandas are adapting to the furnace around them.
+	A lot have become Samurais in the fight to survive. Few were infected by failed science experiments and are now known as the elite class. 
+	Welcome to change and welcome to Pandamania X! This is a 1600 supply collection, with multi-art styles (type): Samurai (800), Mundane (600),
+	and Mutant (200). Pandamania X main goal is to consolidate 3 of our collections into 1 collection. 
 	<br/>
-	- Burn 6000 Hashmingos into 600 PMX Samurais
-	- Burn 500 Lady Mingos into 165 PMX Samurais
-	- Swap 1800 Panda Syndicates into 450 PMX Mundanes
-	- Diamond, Celestial Tunnel, and Money Rug trait Pandas get 1 free claim of Mundane or Samurai Panda.
-	- Roles that receive PMX Mutant Airdrop: Elite Mutant (for each 10 held), Elite Panda (1).  
+		<li> - Burn 6000 Hashmingos into 600 PMX Samurais</li>
+		<li> - Burn 500 Lady Mingos into 165 PMX Samurais</li>
+		<li> - Swap 1800 Panda Syndicates into 450 PMX Mundanes</li>
+		<li> - Diamond, Celestial Tunnel, and Money Rug trait Pandas get 1 free claim of Mundane or Samurai Panda.</li>
+		<li> - Roles that receive PMX Mutant Airdrop: Elite Mutant (for each 10 held), Elite Panda (1).</li>
               <br/>
               <br/>
               </p>
