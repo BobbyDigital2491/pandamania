653ec78954677512ade84546ebebe7e0ae9c62c8
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-03-06 21:21:42 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -9,46 +9,31 @@ export default function Gang() {
 	<div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
   
   <img src="22.png" width="900" height="400" className="align-center"/>
-  <h1 className="text-5xl font-bold leading-none text-center">Change is here!!!</h1>
-
-		<p className="pt-2 pb-8 text-xl font-medium text-center">
-		 Something is happening within Pandamania. Things seem to be evolving and its world has turned 
-		 into the survival of the fittest. No one can say whether this change is good or bad, only 
-		 one thing for certain... Nothing will ever be the same. Welcome to Pandamania X!
-		</p>
-		<br/>
-		<h1 className="text-3xl font-bold leading-none text-center">PMX Gaming</h1>
-		<p className="pt-2 pb-8 text-xl font-medium text-center">
-			Pandamania has accomplished everything on thier previous roadmap and now they're on to 
-			something new. A new journey that is exciting, fun and introduces even better hodler rewards.
-			PMX Gaming is the new focus for Pandamania! Please check out our execution plan to learn more.
-			<br/><br/>
-			<p className=" text-xl font-medium text-center underline underline-offset-8">
-			Click The Following Link For More Information
-			</p>
-			<a href="https://docs.google.com/presentation/d/1L-lfoBEI4g52WdG1JquwUlSPTV0WKs9L02uwxyDei7k/edit?usp=sharing"
-		   className="text-xl leading-none text-center hover:bg-purple-700 ">
-			PMX Gaming Execution Plan
-			</a>
-			</p>
-		<h1 className="text-3xl font-bold leading-none text-center">PMX Collections and Point System</h1>
-		<p className="pt-2 pb-8 text-xl font-medium text-center">
-			PMX Collections consist of 3 different art bases, Samurai, Mutants, and Mundanes. With a collection size of 2,222 that will
-			be airdropped or claimed through burning the older collections. We have even prepared a reward system that helps incentivize
-			the burning process. 
-			<br/><br/>
-			<p className=" text-xl font-medium text-center underline underline-offset-8">
-			Please check out our PMX Collection and Point Release System
-			</p>
-		<a href="https://docs.google.com/presentation/d/1PJ29OPVj4F-QfMa5BIXRzfTsRKntNkm6EakjbTao_Ls/edit?usp=sharing"
-		   className="text-xl leading-none text-center align-middle hover:bg-purple-700 ">
-			PMX Collection and Point Release System
-			</a>
-			</p>
-			
+  
+<h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Change is coming to Pandamania</h1>
+<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+	Something is happening within Pandamania. Things seem to be evolving and its world turned 
+	into survival of the fittest. No one can say whether this change is good or bad only one thing is for certain.
+	Nothing will ever be the same. Welcome to Pandamania X!!!</p>
+	<img src="Pmap.png" width="900" height="400" className="align-center"/>
+	<br/>
+<h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">PMX Gaming</h1>
+<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+	Pandamania has accomplished everything on thier previous roadmap and now they're on to something new.
+	A new journey that is exciting, fun and introduces even better hodler rewards. PMX Gaming is the new 
+	focus for Pandamania!!! Please check our execution plan to learn more</p>
+	<a href="https://docs.google.com/presentation/d/1L-lfoBEI4g52WdG1JquwUlSPTV0WKs9L02uwxyDei7k/edit?usp=sharing" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">PMX Execution Plan</a>
+	<br/>
+	<h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">PMX Collections and Point System</h1>
+<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+	PMX Collections consist of 3 different art bases, Samurai, Mutants, and Mundanes. With a collection size of
+    2,222 that will be air droped or claimed through burning the older collections. We have even prepared a reward system that helps incentivize the burning process. </p>
+	<a href="https://docs.google.com/presentation/d/1PJ29OPVj4F-QfMa5BIXRzfTsRKntNkm6EakjbTao_Ls/edit?usp=sharing" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">PMX Collection and Point Release System</a> 
 	
-		
-	   		 <br/><br/>
+
+
+
+
 	    </div>
 	   
     </section>
@@ -59,4 +44,5 @@ export default function Gang() {
   </>
   );
 }
+
 
