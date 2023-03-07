d8cb6cb9bfa6296b691de1b31c9d7eb649e4b25b
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-03-06 20:40:37 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -38,14 +38,18 @@ export default function Gang() {
 			the burning process. 
 			<br/><br/>
 			</p>
-		<p className=" text-xl font-medium text-center underline underline-offset-8">
-			Please check out our PMX Collection and Point Release System
-			</p>
+		
 	
 		
 	   		 <br/><br/>
 	    </div>
-	   
+	   <p className=" text-xl font-medium text-center underline underline-offset-8">
+			Please check out our PMX Collection and Point Release System
+			</p>
+		<a href="https://docs.google.com/presentation/d/1PJ29OPVj4F-QfMa5BIXRzfTsRKntNkm6EakjbTao_Ls/edit?usp=sharing"
+		   className="text-xl leading-none text-center hover:bg-purple-700 ">
+			PMX Collection and Point Release System
+			</a>
     </section>
 
 
