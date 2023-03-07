e7dcf31a0c0ce8d71267bdbd0ba62643eff6dddf
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-03-06 19:54:25 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -12,7 +12,9 @@ export default function Gang() {
   <h1 className="text-5xl font-bold leading-none text-center">Change is here!!!</h1>
 
 	<p className="pt-2 pb-8 text-xl font-medium text-center">
-    	 
+    	 Something is happening within Pandamania. Things seem to be evolving and its world has turned 
+	 into the survival of the fittest. No one can say whether this change is good or bad, only 
+	 one thing for certain... Nothing will ever be the same. Welcome to Pandamania X!
 		</p>
 	    
 	    </div>
