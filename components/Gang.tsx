d43363e3aec5c5e511fceda8e2dbff0b4051bc6f
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-03-06 20:32:19 -0500

    Update Gang.tsx

--- components/Gang.tsx
+++ components/Gang.tsx
@@ -11,10 +11,10 @@ export default function Gang() {
   <img src="22.png" width="900" height="400" className="align-center"/>
   <h1 className="text-5xl font-bold leading-none text-center">Change is here!!!</h1>
 
-	<p className="pt-2 pb-8 text-xl font-medium text-center">
-    	 Something is happening within Pandamania. Things seem to be evolving and its world has turned 
-	 into the survival of the fittest. No one can say whether this change is good or bad, only 
-	 one thing for certain... Nothing will ever be the same. Welcome to Pandamania X!
+		<p className="pt-2 pb-8 text-xl font-medium text-center">
+		 Something is happening within Pandamania. Things seem to be evolving and its world has turned 
+		 into the survival of the fittest. No one can say whether this change is good or bad, only 
+		 one thing for certain... Nothing will ever be the same. Welcome to Pandamania X!
 		</p>
 		<br/>
 		<h1 className="text-3xl font-bold leading-none text-center">PMX Gaming</h1>
@@ -25,13 +25,27 @@ export default function Gang() {
 			<br/><br/>
 			<p className=" text-xl font-medium text-center underline underline-offset-8">
 			Click The Following Link For More Information
-			
 			</p>
 			<a href="https://docs.google.com/presentation/d/1L-lfoBEI4g52WdG1JquwUlSPTV0WKs9L02uwxyDei7k/edit?usp=sharing"
 		   className="text-xl leading-none text-center hover:bg-purple-700 ">
 			PMX Gaming Execution Plan
 			</a>
 			</p>
+		<h1 className="text-3xl font-bold leading-none text-center">PMX Collections and Point System</h1>
+		<p className="pt-2 pb-8 text-xl font-medium text-center">
+			PMX Collections consist of 3 different art bases, Samurai, Mutants, and Mundanes. With a collection size of 2,222 that will
+			be airdropped or claimed through burning the older collections. We have even prepared a reward system that helps incentivize
+			the burning process. 
+			<br/><br/>
+			<p className=" text-xl font-medium text-center underline underline-offset-8">
+			Please check out our PMX Collection and Point Release System
+			</p>
+			<a href="https://docs.google.com/presentation/d/1PJ29OPVj4F-QfMa5BIXRzfTsRKntNkm6EakjbTao_Ls/edit?usp=sharing"
+		   className="text-xl leading-none text-center hover:bg-purple-700 ">
+			PMX Collection and Point Release System
+			</a>
+			
+		</p>
 		
 	   		 <br/><br/>
 	    </div>
